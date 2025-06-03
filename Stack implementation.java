9db7e21b5ab81610bf4c0b8410ee9e776591309c
nandhini89199 <nandhinipotu12@gmail.com>
2025-06-03 16:01:45 +0530

    Add files via upload

---
+++ Stack implementation.java
@@ -0,0 +1,10 @@
+import java.util.Stack;
+public class Main
+{
+	public static void main(String[] args) {
+	    Stack s=new Stack<>();
+	    s.push(1);
+	    s.push(2);
+		System.out.println(s.peek());
+	}
+}
