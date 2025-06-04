f4d81e46ea75adeeed55efb30696b304dac2c158
nandhini89199 <nandhinipotu12@gmail.com>
2025-06-04 15:20:44 +0530

    Add files via upload

---
+++ deleted at the begining.java
@@ -0,0 +1,68 @@
+class Node
+{
+    int data;
+    Node next;
+    Node(int n)
+    {
+        this.data=n;
+        this.next=null;
+    }
+}
+class LinkedList
+{
+    Node head=null;
+    void add(int n)
+    {
+        Node newNode=new Node(n);
+        if(head==null)
+        {
+            head=newNode;
+        }
+        else{
+            Node curr=head;
+            while(curr.next!=null)
+            {
+                curr=curr.next;
+            }
+            curr.next=newNode;
+        }
+    }
+    void addf(int n)
+    {
+        Node newNode=new Node(n);
+        newNode.next=head;
+        this.head=newNode;
+    }
+void deleteb()
+    {
+        Node curr=head;
+        while(curr.next.next!=null)
+        {
+            curr=curr.next;
+        }
+        curr.next=null;
+    }
+    void display()
+    {
+        Node curr=head;
+        while(curr!=null)
+        {
+            System.out.println(curr.data);
+            curr=curr.next;
+        }
+    }
+}
+public class Main
+{
+	public static void main(String[] args) {
+	 LinkedList l=new LinkedList();
+	 l.add(1);
+	 l.add(2);
+	 l.add(3);
+	 l.addf(4);
+	 l.addf(5);
+	 l.deleteb();
+	 l.deleteb();
+	 l.display();
+}
+}
