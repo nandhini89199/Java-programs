b7878ffee699859d4df169dbab7a5cbcabb341e3
nandhini89199 <nandhinipotu12@gmail.com>
2025-06-04 15:19:36 +0530

    Add files via upload

---
+++ head in the lost.java
@@ -0,0 +1,49 @@
+class Node
+{
+    int data;
+    Node next;
+    Node(int n)
+    {
+        this.data=n;
+        this.next=null;
+    }
+}
+class LinkedList
+{
+    Node head=null;
+    void add(int n)
+    {
+        Node newNode=new Node(n);
+        if(head==null)
+        {
+            head=newNode;
+        }
+        else{
+            Node curr=head;
+            while(curr.next!=null)
+            {
+                curr=curr.next;
+            }
+            curr.next=newNode;
+        }
+    }
+    void display()
+    {
+        Node curr=head;
+        while(curr!=null)
+        {
+            System.out.println(curr.data);
+            curr=curr.next;
+        }
+    }
+}
+public class Main
+{
+	public static void main(String[] args) {
+	 LinkedList l=new LinkedList();
+	 l.add(1);
+	 l.add(2);
+	 l.add(3);
+	 l.display();
+	}
+}
